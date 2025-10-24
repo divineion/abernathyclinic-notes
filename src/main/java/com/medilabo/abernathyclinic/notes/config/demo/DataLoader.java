dbda99a39ce5642e9fe8976042729fbf2239d0cb
divineion <najima.guermoudi@gmail.com>
2025-10-25 00:48:29 +0200

    refactor(demo-data): simplify code and prevent duplicates
     - add private static variables for patient and doctor IDs;
     - instantiate demo notes using new Note constructor;
     - add `addIfNotExists(Note, List<Note>)` helper method that checks for
    existing notes before adding to the notes list to avoid duplicates;
     - persist all notes using `repository.insert().block()`.

--- src/main/java/com/medilabo/abernathyclinic/notes/config/demo/DataLoader.java
+++ src/main/java/com/medilabo/abernathyclinic/notes/config/demo/DataLoader.java
@@ -1,110 +1,101 @@
 package com.medilabo.abernathyclinic.notes.config.demo;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.medilabo.abernathyclinic.notes.entity.Note;
+import com.medilabo.abernathyclinic.notes.repository.CustomizedNoteRepositoryImpl;
 import com.medilabo.abernathyclinic.notes.repository.NoteRepository;
 
+import reactor.core.publisher.Mono;
+
+@Profile("dev")
 @Component
 public class DataLoader implements CommandLineRunner{
 	private NoteRepository repository;
+	private CustomizedNoteRepositoryImpl customRepository;
 
-
-	public DataLoader(NoteRepository repository) {
+	public DataLoader(NoteRepository repository, CustomizedNoteRepositoryImpl customRepository) {
 		this.repository = repository;
+		this.customRepository = customRepository;
 	}
+	
+	private static final String PATIENT_1_ID = "1";
+	private static final String PATIENT_2_ID = "2";
+	private static final String PATIENT_3_ID = "3";
+	private static final String PATIENT_4_ID = "4";
+
+	private static final String DOCTOR_1_ID = "4";
+	private static final String DOCTOR_2_ID = "5";
 
 	@Override
 	public void run(String... args) throws Exception {
 		List<Note> notes = new ArrayList<>();
-		Note note1 = new Note();
-		note1.setPatientId("1");
-		note1.setCreatedAt(LocalDateTime.now());
-		note1.setContent("Le patient déclare qu'il 'se sent très bien' \n "
-				+ "Poids égal ou inférieur au poids recommandé");
-		note1.setDoctorId("4");
-		
-		notes.add(note1);
-		
-		Note note2 = new Note();
-		note2.setPatientId("2");
-		note2.setCreatedAt(LocalDateTime.now().minusMonths(3L));
-		note2.setContent("Le patient déclare qu'il ressent beaucoup de stress au travail \n "
-				+ "Il se plaint également que son audition est anormale dernièrement");
-		note2.setDoctorId("4");
-		
-		notes.add(note2);
-
-		Note note3 = new Note();
-		note3.setPatientId("2");
-		note3.setCreatedAt(LocalDateTime.now());
-		note3.setContent("Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois \n"
-				+ " Il remarque également que son audition continue d'être anormale");
-		note3.setDoctorId("4");
-		
-		notes.add(note3);
 				
-		Note note4 = new Note();
-		note4.setPatientId("3");
-		note4.setCreatedAt(LocalDateTime.now().minusMonths(6L));
-		note4.setContent("Le patient déclare qu'il fume depuis peu");
-		note4.setDoctorId("5");
-		
-		notes.add(note4);
+		Note testNote1 = new Note(PATIENT_1_ID, DOCTOR_1_ID, LocalDateTime.of(2025, Month.AUGUST, 25, 10, 40), null, "Le patient déclare qu'il 'se sent très bien' \n "
+						+ "Poids égal ou inférieur au poids recommandé");
+		addIfNotExists(testNote1, notes);
 
-		Note note5 = new Note();
-		note5.setPatientId("3");
-		note5.setCreatedAt(LocalDateTime.now().minusDays(3L));
-		note5.setContent("Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière  \n "
+		Note testNote2 = new Note(PATIENT_2_ID, DOCTOR_1_ID, LocalDateTime.of(2025, Month.AUGUST, 25, 10, 40), null, 
+				"Le patient déclare qu'il ressent beaucoup de stress au travail \n "
+				+ "Il se plaint également que son audition est anormale dernièrement");
+		addIfNotExists(testNote2, notes);
+				
+		Note testNote3 = new Note(PATIENT_2_ID, DOCTOR_1_ID, LocalDateTime.of(2025, Month.OCTOBER, 25, 15, 10), null, 
+				"Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois \n"
+				+ " Il remarque également que son audition continue d'être anormale");
+		addIfNotExists(testNote3, notes);
+		
+		Note testNote4 = new Note(PATIENT_3_ID, DOCTOR_2_ID, LocalDateTime.of(2023, Month.AUGUST, 22, 14, 15), null, 
+				"Le patient déclare qu'il qu'il fume depuis peu");
+		addIfNotExists(testNote4, notes);
+							
+		Note testNote5 = new Note(PATIENT_3_ID, DOCTOR_2_ID, LocalDateTime.of(2025, Month.FEBRUARY, 25, 14, 40), null, 
+				"Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière  \n "
 				+ "Il se plaint également de crises d’apnée respiratoire anormales \n "
 				+ "Tests de laboratoire indiquant un taux de cholestérol LDL élevé");
-		note5.setDoctorId("5");
+		addIfNotExists(testNote5, notes);
 		
-		notes.add(note5);
-
-		Note note6 = new Note();
-		note6.setPatientId("4");
-		note6.setCreatedAt(LocalDateTime.now().minusMonths(11L));
-		note6.setContent(" Le patient déclare qu'il lui est devenu difficile de monter les escaliers \n "
-				+ "Il se plaint également d’être essoufflé \n "
+		Note testNote6 = new Note(PATIENT_4_ID, DOCTOR_2_ID, LocalDateTime.of(2023, Month.AUGUST, 25, 14, 40), null, 
+				"Le patient déclare qu'il lui est devenu difficile de monter les escaliers \n"
+				+ "Il se plaint également d’être essoufflé \n"
 				+ "Tests de laboratoire indiquant que les anticorps sont élevés \n"
-				+ " Réaction aux médicaments");
-		note6.setDoctorId("6");
+				+ "Réaction aux médicaments");		
+		addIfNotExists(testNote6, notes);
 		
-		notes.add(note6);
-
-		Note note7 = new Note();
-		note7.setPatientId("4");
-		note7.setCreatedAt(LocalDateTime.now().minusMonths(8L));
-		note7.setContent("Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps");
-		note7.setDoctorId("6");
+		Note testNote7 = new Note(PATIENT_4_ID, DOCTOR_2_ID, LocalDateTime.of(2023, Month.DECEMBER, 25, 14, 40), null, 
+				"Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps");
+		addIfNotExists(testNote7, notes);
+				
+		Note testNote8 = new Note(PATIENT_4_ID, DOCTOR_2_ID, LocalDateTime.of(2024, Month.MAY, 25, 9, 15), null,
+				"Le patient déclare avoir commencé à fumer depuis peu \n"
+						+ "Hémoglobine A1C supérieure au niveau recommandé");
+		addIfNotExists(testNote8, notes);
 		
-		notes.add(note7);
-		
-		Note note8 = new Note();
-		note8.setPatientId("4");
-		note8.setCreatedAt(LocalDateTime.now().minusMonths(1L));
-		note8.setContent("Le patient déclare avoir commencé à fumer depuis peu \n "
-				+ "Hémoglobine A1C supérieure au niveau recommandé");
-		note8.setDoctorId("6");
-		
-		notes.add(note8);
-
-		Note note9 = new Note();
-		note9.setPatientId("4");
-		note9.setCreatedAt(LocalDateTime.now().minusDays(2));
-		note9.setContent("Taille, Poids, Cholestérol, Vertige et Réaction");
-		note9.setDoctorId("6");
-		
-		notes.add(note9);
-		
-		// bloquer le flux pour que les données soient créées avant l'initialisation 
-		 notes.forEach(note -> repository.insert(note).block());
-		
+		Note testNote9 = new Note(PATIENT_4_ID, DOCTOR_2_ID, LocalDateTime.of(2025, Month.AUGUST, 18, 14, 40), null, 
+				"Taille, Poids, Cholestérol, Vertige et Réaction");
+		addIfNotExists(testNote9, notes);
+				
+		notes.forEach((note) -> {
+			try { 
+		// block() termione la chaîne des traitements asynchrones et retourne la valeur
+		// je bloque le flux pour que les données soient créées avant l'initialisation de l'app 
+				repository.insert(note).block();
+			} catch (Exception e) {
+				System.err.print("Insertion error for note " + note);
+			}
+		});
+	}
+	
+	private void addIfNotExists(Note note, List<Note> notes) {
+			customRepository.findByPatientIdAndCreatedAt(note.getPatientId(), note.getCreatedAt())
+				.switchIfEmpty(Mono.fromRunnable(() -> notes.add(note)))
+				.block();
 	}
 }
