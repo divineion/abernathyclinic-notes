07282d86ab50cb814a6b8af088f18e497f0fe4e3
divineion <najima.guermoudi@gmail.com>
2025-10-28 22:22:03 +0100

    feat(notes): expose endpoints for notes
     - GET "/api/note/{id}",
     - GET "/api/note/patient/{patientUuid},
     - POST "/api/note/patient/{patientUuid}.
    
    Each endpoint return ResponseEntity wrapping Flux or Mono.

---
+++ src/main/java/com/medilabo/abernathyclinic/notes/controller/NoteController.java
@@ -0,0 +1,40 @@
+package com.medilabo.abernathyclinic.notes.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medilabo.abernathyclinic.notes.dto.CreateNoteDto;
+import com.medilabo.abernathyclinic.notes.entity.Note;
+import com.medilabo.abernathyclinic.notes.service.NoteService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class NoteController {
+	private final NoteService noteService;		
+	
+	public NoteController(NoteService noteService) {
+		this.noteService = noteService;
+	}
+	
+	@GetMapping("/api/note/{id}")
+	public ResponseEntity<Mono<Note>> getNoteById(@PathVariable String id) {
+		return new ResponseEntity<Mono<Note>>(noteService.findById(id), HttpStatus.OK);
+	}
+
+	@GetMapping("/api/note/patient/{patientUuid}")
+	public ResponseEntity<Flux<Note>> getNoteByPatientUuid(@PathVariable String patientUuid) {
+		return new ResponseEntity<Flux<Note>>(noteService.findByPatientUuid(patientUuid), HttpStatus.OK);
+	}
+	
+	@PostMapping("/api/note/patient/{patientUuid}")
+	public ResponseEntity<Mono<Note>> createNote(@PathVariable String patientUuid, @RequestBody CreateNoteDto noteDto) {
+		return new ResponseEntity<Mono<Note>>(noteService.createNote(noteDto), HttpStatus.CREATED);
+	}
+}
