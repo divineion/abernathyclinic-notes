52823bd86071fbfccdc757411296d688eac90bbe
divineion <najima.guermoudi@gmail.com>
2025-11-06 06:35:44 +0100

    feat(dal): add updateNote() in Repository
    Add Mono<UpdateResult> updateNote(String id, UpdateNoteDto noteDto)
    method to CustomizedNoteRepositoryImpl..
    Use `MongoTemplate.updateFirst()` to update a Note content and update
    date.

--- src/main/java/com/medilabo/abernathyclinic/notes/repository/CustomizedNoteRepository.java
+++ src/main/java/com/medilabo/abernathyclinic/notes/repository/CustomizedNoteRepository.java
@@ -2,7 +2,9 @@ package com.medilabo.abernathyclinic.notes.repository;
 
 import java.time.LocalDateTime;
 
+import com.medilabo.abernathyclinic.notes.dto.UpdateNoteDto;
 import com.medilabo.abernathyclinic.notes.entity.Note;
+import com.mongodb.client.result.UpdateResult;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,5 +12,7 @@ import reactor.core.publisher.Mono;
 public interface CustomizedNoteRepository {
 	Mono<Note> findByPatientUuidAndCreatedAt(String patientUuid, LocalDateTime createdAt);
 
-	Flux<Note> findByPatientUuid(String patientUuid);	
+	Flux<Note> findByPatientUuid(String patientUuid);
+
+	Mono<UpdateResult> updateNote(String id, UpdateNoteDto noteDto);
 }
--- src/main/java/com/medilabo/abernathyclinic/notes/repository/CustomizedNoteRepositoryImpl.java
+++ src/main/java/com/medilabo/abernathyclinic/notes/repository/CustomizedNoteRepositoryImpl.java
@@ -2,12 +2,16 @@ package com.medilabo.abernathyclinic.notes.repository;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
+import com.medilabo.abernathyclinic.notes.dto.UpdateNoteDto;
 import com.medilabo.abernathyclinic.notes.entity.Note;
+import com.mongodb.client.result.UpdateResult;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -31,9 +35,25 @@ public class CustomizedNoteRepositoryImpl implements CustomizedNoteRepository {
 	
 	@Override
 	public Flux<Note> findByPatientUuid(String patientUuid) {
+						
 		Query query = new Query()
-				.addCriteria(Criteria.where("patientUuid").is(patientUuid));
+				.addCriteria(Criteria.where("patientUuid").is(patientUuid))
+
+				// chore(dal): return notes sorted by descending creation date
+				.with(Sort.by(Sort.Direction.DESC, "createdAt"));
 		
 		return template.query(Note.class).matching(query).all();
+	}
+
+	@Override
+	public Mono<UpdateResult> updateNote(String id, UpdateNoteDto noteDto) {
+		Query query = new Query()
+				.addCriteria(Criteria.where("id").is(id));
+		
+		Update update = new Update()
+				.set("content", noteDto.content())
+				.set("updatedAt", LocalDateTime.now()); // pour l'heure UTC
+		
+		return template.updateFirst(query, update, Note.class);
 	}
 }
