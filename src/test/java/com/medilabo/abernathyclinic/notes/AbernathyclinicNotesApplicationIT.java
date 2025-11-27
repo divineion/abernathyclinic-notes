ab09806e18e2868e607f1d15dc32c24e5c5e6451
divineion <najima.guermoudi@gmail.com>
2025-11-27 01:08:11 +0100

    fix(ci): rename AbernathyclinicNotesApplicationTests
    Rename to AbernathyclinicNotesApplicationIT to prevent the test to be
    run in the `mvn test` phase

--- src/test/java/com/medilabo/abernathyclinic/notes/AbernathyclinicNotesApplicationIT.java
+++ src/test/java/com/medilabo/abernathyclinic/notes/AbernathyclinicNotesApplicationIT.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class AbernathyclinicNotesApplicationTests {
+class AbernathyclinicNotesApplicationIT {
 
 	@Test
 	void contextLoads() {
